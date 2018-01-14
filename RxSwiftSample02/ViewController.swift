8fdbb8e626974574322c71e791ba1dcd2a9dec3c
Shizuna Ito <adfa1728@chiba-u.jp>
2018-01-14 18:24:44 +0900

    add table view controller

--- RxSwiftSample02/ViewController.swift
+++ RxSwiftSample02/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import RxSwift
+import RxDataSources
 
 class ViewController: UIViewController {
 
+    let disposeBag = DisposeBag()
+    let breadsData = BreadPresenter()
+
+    @IBOutlet weak var breadsTableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let dataSource = RxTableViewSectionedReloadDataSource<SectionModel<String, Bread>>(
+            configureCell: {_, tableView, indexPath, breads in
+                let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+                cell.textLabel?.text = breads.name
+                cell.detailTextLabel?.text = breads.taste
+                cell.imageView?.image = breads.image
+                return cell
+        })
+
+        breadsData.breads.bind(to: breadsTableView.rx.items(dataSource: dataSource)).disposed(by: disposeBag)
+
+        breadsTableView.rx.setDelegate(self).disposed(by: disposeBag)
+
+        dataSource.titleForHeaderInSection = { (dataSource, section: Int) -> String in
+            return dataSource[section].model
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
-
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return CGFloat(65)
+    }
 }
 
